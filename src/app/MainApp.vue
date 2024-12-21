ae95ed915d3135ec6fa8284cce286369d1126e89
vyachean <vyachean@gmail.com>
2024-12-21 14:59:06 +0400

    feature: add material design button and states

--- src/app/MainApp.vue
+++ src/app/MainApp.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 import NotificationList from '../shared/ui/Notifications/NotificationList.vue';
-import { MainView } from '../widgets/MainView';
 </script>
 
 <template>
   <div class="is-flex is-flex-grow-1 is-flex-direction-column is-overflow-auto">
-    <MainView />
+    <RouterView />
   </div>
 
   <NotificationList />
