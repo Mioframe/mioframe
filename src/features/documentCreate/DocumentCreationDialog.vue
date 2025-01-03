719ae0668ed6723981888d93147c11dfe4a98742
vyachean <vyachean@gmail.com>
2025-01-03 22:24:59 +0400

    feat(ui): add material design basic dialog

---
+++ src/features/documentCreate/DocumentCreationDialog.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+import type { DocumentFolder } from '../../shared/lib/cfrDocument';
+import { DATABASE_DOCUMENT_TYPE } from '../../shared/lib/databaseDocument';
+import { MDDialog } from '@shared/ui/Dialog';
+
+const props = defineProps<{
+  folder: DocumentFolder;
+}>();
+
+const emit = defineEmits<{
+  created: [];
+  cancel: [];
+}>();
+
+const stateName = ref<string>();
+
+const onCreate = () => {
+  if (!stateName.value?.length) {
+    throw new Error('name is undefined');
+  }
+
+  props.folder.create({
+    name: stateName.value,
+    type: documentType.value,
+    version: 1,
+  });
+
+  emit('created');
+};
+
+const onCancel = () => {
+  stateName.value = undefined;
+  emit('cancel');
+};
+
+const autofocusElement = ref<HTMLElement>();
+
+watchEffect(() => {
+  autofocusElement.value?.focus();
+});
+
+const documentTypeOptions = [DATABASE_DOCUMENT_TYPE, 'any'] as const;
+
+const documentType = ref<(typeof documentTypeOptions)[number]>(
+  documentTypeOptions[0],
+);
+</script>
+
+<template>
+  <MDDialog
+    headline="Create Document"
+    supporting-text="Think of a name and select the type of the new document."
+    apply-label="Create"
+    cancel-label="Cancel"
+    has-cancel-action
+    @apply="onCreate"
+    @cancel="onCancel"
+  >
+    <div class="field">
+      <label class="label">Name</label>
+
+      <div class="control">
+        <input
+          ref="autofocusElement"
+          v-model="stateName"
+          name="name"
+          class="input"
+          type="text"
+          placeholder="name of the new document"
+          required
+        />
+      </div>
+    </div>
+
+    <div class="select">
+      <select v-model="documentType">
+        <option
+          v-for="option in documentTypeOptions"
+          :key="option"
+          :value="option"
+        >
+          {{ option }}
+        </option>
+      </select>
+    </div>
+  </MDDialog>
+</template>
--- src/shared/ui/Dialog/MDDialog.vue
+++ src/shared/ui/Dialog/MDDialog.vue
@@ -1,14 +1,148 @@
 <script setup lang="ts">
-//
+import { computed } from 'vue';
+import { MDButton } from '../Button';
+
+const props = defineProps<{
+  hide?: boolean;
+  headline: string;
+  supportingText: string;
+  type?: 'basic' | 'full-screen';
+  cancelLabel: string;
+  applyLabel: string;
+  hasCancelAction?: boolean;
+}>();
+
+defineSlots<{
+  default(): unknown;
+  icon(): unknown;
+}>();
+
+defineEmits<{
+  cancel: [];
+  apply: [];
+}>();
+
+const dialogType = computed(() => props.type ?? 'basic');
 </script>
 
 <template>
-  <dialog open>
-    <div class="dialog">//</div>
+  <dialog
+    :open="!hide"
+    class="md-dialog md-dialog__scrim"
+    :class="[
+      {
+        'md-dialog_has-icon': !!$slots.icon,
+      },
+      `md-dialog_${dialogType}-type`,
+    ]"
+  >
+    <div class="md-dialog__container">
+      <div v-if="!!$slots.icon" class="md-dialog__icon">
+        <slot name="icon" />
+      </div>
+
+      <div class="md-dialog__headline">
+        {{ headline }}
+      </div>
+
+      <div class="md-dialog__supporting-text">
+        {{ supportingText }}
+      </div>
+
+      <div v-if="!!$slots.default" class="md-dialog__body">
+        <slot />
+      </div>
+
+      <div class="md-dialog__actions">
+        <MDButton
+          v-if="hasCancelAction"
+          :label="cancelLabel"
+          type="text"
+          @click="$emit('cancel')"
+        />
+
+        <MDButton :label="applyLabel" type="text" @click="$emit('apply')" />
+      </div>
+    </div>
   </dialog>
 </template>
 
 <style scoped>
-.dialog {
+.md-dialog {
+  &__scrim {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+  }
+
+  &__container {
+    box-sizing: border-box;
+    padding: 24px;
+    border-radius: 28px;
+    height: fit-content;
+    max-height: 100dvh;
+    overflow-y: auto;
+    min-width: 280px;
+    max-width: min(560px, 100dvw);
+    width: fit-content;
+
+    background-color: var(--md-sys-color-surface-container-high);
+    box-shadow: var(--md-sys-elevation-level3);
+  }
+
+  &__icon {
+    width: 24px;
+    height: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--md-sys-color-secondary);
+  }
+
+  &__headline {
+    text-align: start;
+
+    font-family: var(--md-sys-typescale-headline-small-font);
+    line-height: var(--md-sys-typescale-headline-small-line-height);
+    font-size: var(--md-sys-typescale-headline-small-size);
+    font-weight: var(--md-sys-typescale-headline-small-weight);
+    letter-spacing: var(--md-sys-typescale-headline-small-tracking);
+
+    color: var(--md-sys-color-on-surface);
+
+    .md-dialog_has-icon & {
+      text-align: center;
+    }
+  }
+
+  &__supporting-text {
+    margin-top: 16px;
+
+    font-family: var(--md-sys-typescale-body-medium-font);
+    line-height: var(--md-sys-typescale-body-medium-line-height);
+    font-size: var(--md-sys-typescale-body-medium-size);
+    font-weight: var(--md-sys-typescale-body-medium-weight);
+    letter-spacing: var(--md-sys-typescale-body-medium-tracking);
+
+    color: var(--md-sys-color-on-surface-variant);
+  }
+
+  &__body {
+    margin-top: 24px;
+  }
+
+  &__actions {
+    margin-top: 24px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    gap: 8px;
+  }
 }
 </style>
