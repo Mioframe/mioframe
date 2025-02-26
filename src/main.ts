a4dac1c99775a4b404359f2b013a417145b17457
vyachean <vyachean@gmail.com>
2025-02-27 00:10:34 +0400

    feat: implement root element setup and improve document explorer layout

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { setupRootElement } from '@shared/lib/useRootElement';
 import { setupApp } from './app/setupApp';
 
-setupApp().mount('#app');
+const rootMountElement = document.getElementById('app');
+
+if (rootMountElement) {
+  const app = setupApp();
+  app.mount(rootMountElement);
+  setupRootElement(app, rootMountElement);
+}
 
 if (import.meta.env.DEV) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- for develop
   // @ts-ignore
-  import('./temp');
+  void import('./temp');
 }
