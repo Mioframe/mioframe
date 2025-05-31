804ca4a49e722f8111897785f7c4771150827021
vyachean <vyachean@gmail.com>
2025-05-31 12:08:07 +0400

    refactor: update setupApp to accept app instance, remove unused Pinia import
    style: comment out global background color in index.css for better component-specific styling
    
    fix: update targetElement prop type in MDPlainTooltip to allow null
    
    delete: remove unused MdRichTooltip component
    
    chore: update tsconfig.app.json to streamline paths and include settings
    
    chore: add env.d.ts for type references
    
    chore: initialize Histoire configuration for Vue components
    
    chore: setup Histoire with Vue 3 in histoire.setup.ts
    
    docs: add story for MDPlainTooltip component with usage examples
    
    feat: add MDRichTooltip component with structured template and styles

---
+++ src/shared/ui/Tooltips/MDPlainTooltip.story.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useTemplateRef } from 'vue';
+import MDPlainTooltip from './MDPlainTooltip.vue';
+
+const targetEl = useTemplateRef('targetEl');
+</script>
+
+<template>
+  <Story title="Plain Tooltip">
+    <Variant title="easy to using">
+      <button type="button">
+        button with Plain Tooltip
+        <MDPlainTooltip text="tooltip text" />
+      </button>
+    </Variant>
+
+    <Variant title="use with target">
+      <button ref="targetEl" type="button">button with Plain Tooltip</button>
+
+      <MDPlainTooltip :target-element="targetEl" text="tooltip text" />
+    </Variant>
+  </Story>
+</template>
+
+<docs lang="md">
+# Plain Tooltip
+
+Подсказка при наведении
+
+Соответствует [Tooltips - Material Design 3](https://m3.material.io/components/tooltips/overview)
+
+## Интеграция
+
+По умолчанию регистрируется на наведение родительского элемента.
+
+В параметрах можно указать ссылку на целевой элемент.
+
+## Поведение
+
+Самостоятельно определяет наведение и выбирает предпочтительную сторону отображения учитывая размер содержимого и расположение в окне браузера.
+</docs>
