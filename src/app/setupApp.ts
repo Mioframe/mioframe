16b9e85a10cfe7b3f67ac7998feb8eb5c6be7d71
vyachean <vyachean@gmail.com>
2025-05-31 12:08:07 +0400

    refactor: update setupApp to accept app instance, remove unused Pinia import
    style: comment out global background color in index.css for better component-specific styling
    
    fix: update targetElement prop type in MDPlainTooltip to allow null
    
    delete: remove unused MdRichTooltip component
    
    chore: update tsconfig.app.json to streamline paths and include settings
    
    chore: add env.d.ts for type references
    
    chore: initialize Histoire configuration for Vue components
    
    chore: setup Histoire with Vue 3 in histoire.setup.ts
    
    docs: add story for MDPlainTooltip component with usage examples
    
    feat: add MDRichTooltip component with structured template and styles

--- src/app/setupApp.ts
+++ src/app/setupApp.ts
@@ -1,29 +1,25 @@
+import type { App } from 'vue';
 import { createApp } from 'vue';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/styles.css';
 import MainApp from './MainApp.vue';
-import { createPinia } from 'pinia';
 import { createHead } from '@unhead/vue/client';
 import { router } from './router';
 
 /**
  * Инициализация и настройка Vue приложения
  */
-export const setupApp = async () => {
-  const app = createApp(MainApp);
-
+export const setupApp = async (app: App = createApp(MainApp)) => {
   const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 
   if (SENTRY_DSN?.length && import.meta.env.PROD) {
-    const { setupSentry } = await import('./setupSentry')
+    const { setupSentry } = await import('./setupSentry');
     setupSentry(app, SENTRY_DSN);
   }
 
   app.use(router);
 
   app.use(createHead());
-
-  app.use(createPinia());
 
   return app;
 };
