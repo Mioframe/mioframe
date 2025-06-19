0434b674f4ad5d4478df2d7d0838dd245b8f656c
vyachean <vyachean@gmail.com>
2025-06-19 13:00:58 +0400

    Remove histoire setup file for Vue 3 integration

--- src/histoire.setup.ts
+++
@@ -1,6 +0,0 @@
-import { defineSetupVue3 } from '@histoire/plugin-vue';
-import { setupApp } from './app/setupApp';
-
-export const setupVue3 = defineSetupVue3(async ({ app }) => {
-  await setupApp(app);
-});
