73fece154ab3fa4a33e76b4a1d95674591ec921f
vyachean <vyachean@gmail.com>
2025-07-08 10:30:54 +0400

    fix(performanceOverlay): simplify conditionals for performance metrics display

--- src/shared/ui/performance/PerformanceOverlay.vue
+++ src/shared/ui/performance/PerformanceOverlay.vue
@@ -11,46 +11,50 @@ const buildDate = new Date(__BUILD_DATE__).toLocaleString();
     <div class="performance-overlay__item">v: {{ buildDate }}</div>
 
     <div
+      v-if="fps !== undefined"
       class="performance-overlay__item"
       :class="{
-        'performance-overlay_alarm': 30 > (fps ?? 0),
-        'performance-overlay_warn': 45 > (fps ?? 0),
+        'performance-overlay_alarm': 30 > fps,
+        'performance-overlay_warn': 45 > fps,
       }"
     >
       fps:{{ fps }}
     </div>
 
     <div
+      v-if="cls !== undefined"
       class="performance-overlay__item"
       :class="{
-        'performance-overlay_alarm': 0.1 < (cls ?? 0),
-        'performance-overlay_warn': 0.05 < (cls ?? 0),
+        'performance-overlay_alarm': 0.1 < cls,
+        'performance-overlay_warn': 0.05 < cls,
       }"
     >
       cls:{{ cls }}
     </div>
 
     <div
+      v-if="inp !== undefined"
       class="performance-overlay__item"
       :class="{
-        'performance-overlay_alarm': 200 < (inp ?? 0),
-        'performance-overlay_warn': 100 < (inp ?? 0),
+        'performance-overlay_alarm': 200 < inp,
+        'performance-overlay_warn': 100 < inp,
       }"
     >
       inp:{{ inp }}
     </div>
 
     <div
+      v-if="lcp !== undefined"
       class="performance-overlay__item"
       :class="{
-        'performance-overlay_alarm': 2200 < (lcp ?? 0),
-        'performance-overlay_warn': 1100 < (lcp ?? 0),
+        'performance-overlay_alarm': 2200 < lcp,
+        'performance-overlay_warn': 1100 < lcp,
       }"
     >
       lcp:{{ lcp }}
     </div>
 
-    <div class="performance-overlay__item">
+    <div v-if="longTasks.length > 0" class="performance-overlay__item">
       longTasks:
       <pre>{{ longTasks }}</pre>
     </div>
