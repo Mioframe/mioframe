6568da78ea409a9d757c691bdc24eba4656c41b5
vyachean <vyachean@gmail.com>
2025-07-12 11:34:57 +0400

    feat(icon): integrate Material Symbols and refactor icon state management

---
+++ src/shared/ui/Icon/useMaterialDesignSymbols.ts
@@ -0,0 +1,100 @@
+import { createGlobalState, useStorage } from '@vueuse/core';
+import { debounce, merge, uniq } from 'es-toolkit';
+import { ref, shallowReactive, watch } from 'vue';
+import { array, object, string } from 'zod/v4-mini';
+import qs from 'use-qs';
+import { loadStylesheet } from './loadStylesheet';
+
+const searchStylesheetLinks = (href: string) =>
+  document.querySelectorAll(`link[href*="${href}"]`);
+
+const awaitFont = async (fontName: string) => {
+  await document.fonts.load(`1em "${fontName}"`);
+};
+
+export const useIconStates = createGlobalState(() => {
+  const url = `https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap`;
+
+  const symbolsUsed = useStorage<string[]>(
+    'MaterialSymbolsRounded',
+    [],
+    localStorage,
+    { mergeDefaults: merge },
+  );
+  const loadedSymbols = shallowReactive(new Set<string>());
+  const loadingSymbols = shallowReactive(new Set<string>());
+
+  searchStylesheetLinks(url).forEach((el) => {
+    if (el instanceof HTMLLinkElement) {
+      const { data: { icon_names } = {} } = object({
+        icon_names: array(string()),
+      }).safeParse(qs.parse(el.href));
+
+      if (icon_names) {
+        icon_names.forEach((name) => {
+          if (name) {
+            loadedSymbols.add(name);
+          }
+        });
+      }
+    }
+  });
+
+  const isEdgeOnAndroid = (ua = navigator.userAgent) => {
+    const isAndroid = /\bAndroid\b/i.test(ua);
+    const isEdgeA = /\bEdgA\/\d+/i.test(ua);
+    return isAndroid && isEdgeA;
+  };
+
+  const loadedAllSymbols = ref(false);
+
+  const loadFont = async () => {
+    const names = uniq([...loadedSymbols.values(), ...symbolsUsed.value]);
+    if (names.length) {
+      names.sort();
+      names.forEach((name) => {
+        if (!loadedSymbols.has(name)) {
+          loadingSymbols.add(name);
+        }
+      });
+
+      const oldLinks = searchStylesheetLinks(url);
+
+      if (!loadedAllSymbols.value) {
+        if (isEdgeOnAndroid()) {
+          await import('material-symbols/rounded.css');
+          loadedAllSymbols.value = true;
+        } else {
+          await loadStylesheet(`${url}&icon_names=${names.join(',')}`);
+        }
+        await awaitFont('Material Symbols Rounded');
+      }
+
+      oldLinks.forEach((el) => {
+        el.remove();
+      });
+
+      names.forEach((name) => {
+        loadingSymbols.delete(name);
+        loadedSymbols.add(name);
+      });
+    }
+  };
+
+  const debounceLoadFond = debounce(loadFont, 500, {
+    edges: ['leading', 'trailing'],
+  });
+
+  watch(symbolsUsed, debounceLoadFond, { immediate: true, deep: true });
+
+  const addLoadSymbol = (name: string) => {
+    if (!symbolsUsed.value.includes(name)) {
+      symbolsUsed.value.push(name);
+    }
+  };
+
+  return {
+    addLoadSymbol,
+    loadedSymbols,
+  };
+});
