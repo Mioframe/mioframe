c97421e57def14a0674ddb0071b62cc120b9ba30
vyachean <vyachean@gmail.com>
2025-07-17 10:20:57 +0400

    fix: auto hide fab container

--- src/shared/ui/Button/MDFabContainer.vue
+++ src/shared/ui/Button/MDFabContainer.vue
@@ -1,19 +1,69 @@
 <script setup lang="ts">
+import { computed, ref, toRefs } from 'vue';
 import FixedPlaceholder from '../Layers/FixedPlaceholder.vue';
+import { useEventListener, useParentElement } from '@vueuse/core';
+import { isUndefined, throttle } from 'es-toolkit';
+
+const props = defineProps<{
+  autoHide?: boolean;
+}>();
+
+const { autoHide } = toRefs(props);
 
 defineSlots<{
   default(): unknown;
 }>();
+
+const parentEl = useParentElement();
+
+let lastScrollTop: number | undefined = undefined;
+
+const scrollDirection = ref<'top' | 'bottom'>();
+
+useEventListener(
+  parentEl,
+  'scroll',
+  throttle(() => {
+    if (parentEl.value) {
+      if (lastScrollTop && parentEl.value.scrollTop > lastScrollTop) {
+        scrollDirection.value = 'bottom';
+      } else if (lastScrollTop && parentEl.value.scrollTop < lastScrollTop) {
+        scrollDirection.value = 'top';
+      } else {
+        scrollDirection.value = undefined;
+      }
+      lastScrollTop = parentEl.value.scrollTop;
+    }
+  }, 300),
+);
+
+const show = computed(
+  () =>
+    !autoHide.value ||
+    isUndefined(scrollDirection.value) ||
+    scrollDirection.value === 'top',
+);
 </script>
 
 <template>
   <FixedPlaceholder
     class="md-fab-container__placeholder"
     priority-width="placeholder"
+    :class="{
+      'md-fab-container_auto-hide': autoHide,
+    }"
   >
-    <div class="md-fab-container">
-      <slot name="default" />
-    </div>
+    <Transition>
+      <div
+        v-if="show"
+        class="md-fab-container"
+        :class="{
+          'md-fab-container_auto-hide': autoHide,
+        }"
+      >
+        <slot name="default" />
+      </div>
+    </Transition>
   </FixedPlaceholder>
 </template>
 
@@ -36,6 +86,37 @@ defineSlots<{
     position: sticky;
     bottom: 0;
     flex-shrink: 0;
+    margin-right: calc(16px - var(--md-pane-padding, 0));
+  }
+
+  &_auto-hide {
+    &.md-fab-container__placeholder {
+      height: 0;
+    }
+    &.md-fab-container {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+  }
+
+  &.v {
+    &-enter-active,
+    &-leave-active {
+      transition-property: transform, opacity;
+      transition-duration: var(--md-sys-motion-duration-medium1);
+    }
+    &-leave-to,
+    &-enter-from {
+      opacity: 0;
+      transform: translateY(100%) scale(0);
+    }
+
+    &-leave-from,
+    &-enter-to {
+      opacity: 1;
+      transform: translateY(0) scale(1);
+    }
   }
 
   :deep() {
