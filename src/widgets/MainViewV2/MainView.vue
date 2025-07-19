19dcd15410f27b92a7b0c902d1879762a431370d
vyachean <vyachean@gmail.com>
2025-07-19 14:31:54 +0400

    fix: make navigation organized

--- src/widgets/MainViewV2/MainView.vue
+++ src/widgets/MainViewV2/MainView.vue
@@ -6,12 +6,27 @@ import { RepoExplorerPane } from '@widget/MainViewV2/RepoExplorer';
 import { computed } from 'vue';
 import { useRepoExplorerNavigate } from '@widget/MainViewV2/useRepoExplorerNavigate';
 import DocumentViewPane from '@widget/DocumentView/DocumentViewPane.vue';
+import { MDIconButton } from '@shared/ui/Button';
+import { useRouter } from 'vue-router';
 
-const { directoryEntry: currentDirectory, state } = useRepoExplorerNavigate();
+const {
+  directoryEntry: currentDirectory,
+  state,
+  closeDocument,
+} = useRepoExplorerNavigate();
 
 const directory = computed(() => currentDirectory.value);
 
 const documentId = computed(() => state.document);
+
+const onClickCloseDocument = async () => {
+  await closeDocument();
+};
+
+const router = useRouter();
+const onClickBack = () => {
+  router.back();
+};
 </script>
 
 <template>
@@ -22,11 +37,27 @@ const documentId = computed(() => state.document);
       <RepoExplorerPane v-else />
     </template>
 
-    <template v-if="currentDirectory && documentId" #secondPane>
-      <DocumentViewPane
-        :directory="currentDirectory"
-        :document-id="documentId"
-      />
+    <template
+      v-if="currentDirectory && documentId"
+      #secondPane="{ showFirstPane }"
+    >
+      <DocumentViewPane :directory="currentDirectory" :document-id="documentId">
+        <template #leadingNavigation>
+          <MDIconButton
+            v-if="showFirstPane"
+            tooltip="close"
+            md-symbol-name="close"
+            @click="onClickCloseDocument"
+          />
+
+          <MDIconButton
+            v-else
+            tooltip="back"
+            md-symbol-name="arrow_back"
+            @click="onClickBack"
+          />
+        </template>
+      </DocumentViewPane>
     </template>
   </MDMainLayer>
 </template>
