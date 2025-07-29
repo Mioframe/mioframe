60fac241d53a433400abda92367a072e71d65eea
vyachean <vyachean@gmail.com>
2025-07-29 21:10:33 +0400

    feat: refactor boolean property handling, introduce MDCheckboxField, and streamline property creation dialogs

---
+++ src/features/databaseBooleanPropertyEdit/DatabaseBooleanPropertyEditSection.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { BooleanProperty } from '@entity/databaseBoolean';
+import { MDCheckboxField } from '@shared/ui/Checkbox';
+import { computed } from 'vue';
+
+const propertyModel = defineModel<BooleanProperty>('property', {
+  required: true,
+});
+
+const indeterminateModel = computed({
+  get: () => propertyModel.value.indeterminate,
+  set: (indeterminate) =>
+    (propertyModel.value = { ...propertyModel.value, indeterminate }),
+});
+</script>
+
+<template>
+  <section>
+    <MDCheckboxField v-model="indeterminateModel" label="indeterminate" />
+  </section>
+</template>
---
+++ src/features/databaseRelationPropertyEdit/index.ts
@@ -0,0 +1,1 @@
+export { default as DatabaseRelationPropertyEditSection } from './DatabaseRelationPropertyEditSection.vue';
