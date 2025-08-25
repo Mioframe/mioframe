59ce4b1810f31c3aabb25d6922819976efd3ac29
vyachean <vyachean@gmail.com>
2025-08-25 14:24:07 +0400

    feat: add DatabasePropertyEditDialog component and integrate into DatabasePropertiesSheet

--- src/features/databaseRelationPropertyEdit/DatabaseRelationPropertyField.vue
+++ src/features/databaseRelationPropertyEdit/DatabaseRelationPropertyField.vue
@@ -4,13 +4,22 @@ import { type AMDocumentId } from '@shared/lib/cfrDocument';
 import { useDocumentFolder } from '@shared/lib/cfrDocument/useDocumentFolder';
 import type { DirectoryFSEntry } from '@shared/lib/fileSystem';
 import { MDSelect } from '@shared/ui/Select';
-import { computed, reactive, shallowRef, watch } from 'vue';
+import { computed, nextTick, reactive, shallowRef, watch } from 'vue';
 
 const { directory } = defineProps<{
   directory: DirectoryFSEntry;
 }>();
 
 const relationModel = defineModel<Relation>();
+
+const relationDocumentIdModel = computed({
+  get: () => relationModel.value?.documentId,
+  set: (documentId: AMDocumentId) =>
+    (relationModel.value = {
+      ...relationModel.value,
+      documentId,
+    }),
+});
 
 type DatabaseDocumentOption = { label: string; key: AMDocumentId };
 
@@ -24,6 +33,7 @@ const documentMap = computed(() => documentFolder.value?.documentMap);
 
 const documentNamesMap = reactive(new Map<AMDocumentId, string>());
 
+// заполнение documentNamesMap
 watch(
   documentMap,
   (documentMap) => {
@@ -54,9 +64,32 @@ const documentOptionList = computed((): DatabaseDocumentOption[] =>
   ),
 );
 
-watch(selectedDocumentOptions, ([{ key: documentId }]) => {
-  relationModel.value = { ...relationModel.value, documentId };
-});
+const selectedDocumentOptionsWatchHandle = watch(
+  selectedDocumentOptions,
+  ([{ key: documentId }]) => {
+    relationDocumentIdWatchHandle.pause();
+    relationDocumentIdModel.value = documentId;
+    void nextTick(relationDocumentIdWatchHandle.resume);
+  },
+);
+
+const relationDocumentIdWatchHandle = watch(
+  relationDocumentIdModel,
+  (documentId) => {
+    selectedDocumentOptionsWatchHandle.pause();
+    if (documentId) {
+      selectedDocumentOptions.value = [
+        {
+          key: documentId,
+          label: documentNamesMap.get(documentId) ?? 'unknown document name',
+        },
+      ];
+    }
+
+    void nextTick(selectedDocumentOptionsWatchHandle.resume);
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
