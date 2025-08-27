7f0aaabf13d7f6f3ba7d09b5082a9c187958cd52
vyachean <vyachean@gmail.com>
2025-08-27 16:32:48 +0400

    feat: enhance accessibility and improve component structure
    - Updated `CFRDocumentMDListItem.vue` and `FSEntryMDListItem.vue` to pass document and entry names to trailing icon slots for better contextual information.
    - Added `aria-label` attributes to buttons in `MDButton.vue`, `MDFab.vue`, and `MDIconButton.vue` for improved accessibility.
    - Implemented `aria-labelledby` in `MDDialog.vue` to associate dialog titles with their respective dialogs.
    - Set `role` attributes for list components in `MDListContainer.vue` and `MDListItem.vue` to enhance semantic structure.
    - Enhanced context menu buttons in `MDContextMenuButton.vue` and `MDMenu.vue` with `aria-label` for better screen reader support.
    - Added `aria-label` to text fields in `MDTextField.vue` for improved accessibility.
    - Introduced Cypress configuration files and initial tests for application startup and directory management.
    - Created fixture data for testing purposes in `example.json`.
    - Added support commands and global configuration for Cypress in `commands.ts` and `e2e.ts`.
    - Established a pnpm workspace configuration in `pnpm-workspace.yaml`.
    - Created TypeScript definitions for ARIA attributes in `shims-aria.d.ts`.

---
+++ cypress/e2e/1-getting-started/mvp.cy.ts
@@ -0,0 +1,105 @@
+/// <reference types="cypress" />
+
+describe('App Startup', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  afterEach(() => {
+    cy.window().then(async (window) => {
+      if ('serviceWorker' in window.navigator) {
+        const serviceWorkerRegistration =
+          await window.navigator.serviceWorker.getRegistrations();
+        for (const r of serviceWorkerRegistration) {
+          await r.unregister();
+        }
+      }
+    });
+  });
+
+  it('load the app and display OPFS', () => {
+    cy.contains('Origin private file system').should('be.visible');
+  });
+});
+
+describe('Directory management', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  before(() => {
+    cy.window().then(async () => {
+      if ('caches' in window) {
+        const keys = await window.caches.keys();
+        for (const key of keys) await window.caches.delete(key);
+      }
+
+      // Очищаем localStorage и sessionStorage
+      window.localStorage.clear();
+      window.sessionStorage.clear();
+
+      const rootOpfs = await navigator.storage.getDirectory(); // корневая папка OPFS
+      for await (const [name] of rootOpfs.entries()) {
+        await rootOpfs.removeEntry(name, { recursive: true });
+      }
+    });
+  });
+
+  after(() => {
+    cy.window().then(async (window) => {
+      if ('serviceWorker' in window.navigator) {
+        const serviceWorkerRegistration =
+          await window.navigator.serviceWorker.getRegistrations();
+        for (const r of serviceWorkerRegistration) {
+          await r.unregister();
+        }
+      }
+    });
+  });
+
+  const openOPFS = () => {
+    cy.contains('Origin private file system').click();
+  };
+
+  it('open OPFS', () => {
+    openOPFS();
+
+    cy.url().should('include', 'Origin%20private%20file%20system');
+  });
+
+  const createFolder = () => {
+    cy.get('[aria-label="Create directory"]').click();
+
+    const folderName = `test folder ${Date.now()}`;
+
+    cy.get('input[aria-label="Folder\'s name"]').type(folderName);
+
+    cy.get('button[aria-label="Create"]').click();
+
+    return folderName;
+  };
+
+  it('Create first folder', () => {
+    openOPFS();
+
+    const folderName = createFolder();
+
+    cy.get('.document-explorer-widget').should('contain.text', folderName);
+  });
+
+  it('Remove folder', () => {
+    openOPFS();
+
+    const folderName = createFolder();
+
+    cy.get(`[aria-label="options ${folderName}"]`).click();
+
+    cy.get(`[aria-label="options ${folderName} menu"]`)
+      .contains('remove', { matchCase: false })
+      .click();
+
+    cy.get('dialog').contains('button', 'remove', { matchCase: false }).click();
+
+    cy.contains(folderName).should('not.exist');
+  });
+});
