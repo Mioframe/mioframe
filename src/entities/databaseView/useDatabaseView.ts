134865ba1de18eac0a0f23afdb2607e18c3a83c3
vyachean <vyachean@gmail.com>
2025-12-22 13:46:12 +0400

    feat: replace asyncComputed with useLiveResource for improved data handling in useDatabaseView and useDatabaseViews

--- src/entities/databaseView/useDatabaseView.ts
+++ src/entities/databaseView/useDatabaseView.ts
@@ -4,8 +4,8 @@ import type {
   DatabaseView,
   DatabaseViewId,
 } from '@shared/lib/databaseDocument';
+import { useLiveResource } from '@shared/lib/useLiveResource';
 import { useMainService } from '@shared/service';
-import { asyncComputed } from '@vueuse/core';
 import type { Ref } from 'vue';
 
 export const useDatabaseView = (
@@ -15,18 +15,37 @@ export const useDatabaseView = (
 ) => {
   const {
     databaseDocument: {
+      onChangeDocument,
       views: { getView, patch },
     },
   } = useMainService();
 
-  const view = asyncComputed(
-    () => getView(path.value, documentId.value, viewId.value),
-    undefined,
-    { lazy: true },
+  const {
+    errorMessage,
+    isLoading,
+    isReady,
+    state: view,
+  } = useLiveResource(
+    () => ({
+      path: path.value,
+      documentId: documentId.value,
+      viewId: viewId.value,
+    }),
+    {
+      fetch: ({ documentId, path, viewId }) =>
+        getView(path, documentId, viewId),
+      subscribe: ({ documentId, path }, cb) =>
+        onChangeDocument(path, documentId, cb),
+      defaultErrorMessage: 'Error reading view',
+    },
   );
 
   return {
     view,
+    errorMessage,
+    isLoading,
+    isReady,
+
     patch: (view: PatchSource<DatabaseView>) =>
       patch(path.value, documentId.value, viewId.value, view),
   };
