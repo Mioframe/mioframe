a37302118d933a9aad36c6eac10fc3d26ab7b2bb
vyachean <vyachean@gmail.com>
2026-01-20 21:58:48 +0400

    feat: implement MDMenu and MDMenuBase components with improved structure and functionality

--- src/entities/databaseProperty/index.ts
+++ src/entities/databaseProperty/index.ts
@@ -1,3 +1,4 @@
 export { default as DatabasePropertyList } from './DatabasePropertyList.vue';
 export { useDatabaseProperties } from './useDatabaseProperties';
 export { useDatabaseProperty } from './useDatabaseProperty';
+export { default as DatabasePropertyMenuItem } from './DatabasePropertyMenuItem.vue';
