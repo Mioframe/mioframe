9274c69de8f091a836b1c3c3014adf27ff83e7ba
vyachean <vyachean@gmail.com>
2026-01-21 12:36:12 +0400

    feat: add DatabaseFilterAddButton component for managing filters

---
+++ src/features/databaseFilterEdit/DatabaseFilterAddButton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useDatabaseProperties } from '@entity/databaseProperty';
+import type { AMDocumentId } from '@shared/lib/automerge';
+import { MDButton } from '@shared/ui/Button';
+import { MDSymbol } from '@shared/ui/Icon';
+import { MDMenuBase } from '@shared/ui/Menu';
+import type { MaybeElement } from '@vueuse/core';
+import { shallowRef, toRefs, useTemplateRef } from 'vue';
+
+const props = defineProps<{
+  path: string;
+  documentId: AMDocumentId;
+}>();
+
+const { path, documentId } = toRefs(props);
+
+const addButton = useTemplateRef<MaybeElement>('addButton');
+
+const showMenu = shallowRef(false);
+
+const onClickAdd = () => {
+  showMenu.value = true;
+};
+
+const { propertiesIdList } = useDatabaseProperties(path, documentId);
+</script>
+
+<template>
+  <MDButton ref="addButton" label="add filter" @click="onClickAdd">
+    <template #icon>
+      <MDSymbol name="add" />
+    </template>
+  </MDButton>
+
+  <MDMenuBase v-model:show="showMenu" :target="addButton">
+    <template v-for="propertyId in propertiesIdList" :key="propertyId">
+      <!-- <MDMenuItemBase /> -->
+      <!-- todo: создать PropertyMenuItemBase -->
+    </template>
+  </MDMenuBase>
+</template>
