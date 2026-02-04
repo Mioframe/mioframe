9f2ad6ee62df58e7eb9996e1ebcc65ed31919aca
vyachean <vyachean@gmail.com>
2026-02-04 10:00:18 +0400

    chore: configure PWA caching strategy for better performance and resource management

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { VitePWA, cachePreset } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import { fileURLToPath, URL } from 'node:url';
 import { dependencies, devDependencies } from './package.json';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import TurboConsole from 'unplugin-turbo-console/vite';
+
+const daysToSeconds = (days: number) => 24 * 60 * 60 * days;
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, isPreview }) => {
@@ -24,7 +26,103 @@ export default defineConfig(({ mode, isPreview }) => {
               background_color: '#000',
             },
             workbox: {
-              runtimeCaching: cachePreset,
+              runtimeCaching: [
+                {
+                  urlPattern:
+                    /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+                  handler: 'CacheFirst',
+                  options: {
+                    cacheName: 'google-fonts',
+                    expiration: {
+                      maxEntries: 20,
+                      maxAgeSeconds: daysToSeconds(365),
+                    },
+                    cacheableResponse: {
+                      statuses: [0, 200],
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+                  handler: 'StaleWhileRevalidate',
+                  options: {
+                    cacheName: 'static-font-assets',
+                    expiration: {
+                      maxEntries: 10,
+                      maxAgeSeconds: daysToSeconds(30),
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+                  handler: 'StaleWhileRevalidate',
+                  options: {
+                    cacheName: 'static-image-assets',
+                    expiration: {
+                      maxEntries: 64,
+                      maxAgeSeconds: daysToSeconds(14),
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\.js$/i,
+                  handler: 'StaleWhileRevalidate',
+                  options: {
+                    cacheName: 'static-js-assets',
+                    expiration: {
+                      maxEntries: 64,
+                      maxAgeSeconds: daysToSeconds(2),
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\.(?:css|less)$/i,
+                  handler: 'StaleWhileRevalidate',
+                  options: {
+                    cacheName: 'static-style-assets',
+                    expiration: {
+                      maxEntries: 64,
+                      maxAgeSeconds: daysToSeconds(2),
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\.(?:json|xml|csv)$/i,
+                  handler: 'NetworkFirst',
+                  options: {
+                    cacheName: 'static-data-assets',
+                    expiration: {
+                      maxEntries: 32,
+                      maxAgeSeconds: daysToSeconds(7),
+                    },
+                  },
+                },
+                {
+                  urlPattern: /\/api\/.*$/i,
+                  handler: 'NetworkFirst',
+                  method: 'GET',
+                  options: {
+                    cacheName: 'apis',
+                    expiration: {
+                      maxEntries: 16,
+                      maxAgeSeconds: daysToSeconds(1),
+                    },
+                    networkTimeoutSeconds: 10,
+                  },
+                },
+                {
+                  urlPattern: /.*/i,
+                  handler: 'NetworkFirst',
+                  options: {
+                    cacheName: 'others',
+                    expiration: {
+                      maxEntries: 32,
+                      maxAgeSeconds: daysToSeconds(1),
+                    },
+                    networkTimeoutSeconds: 10,
+                  },
+                },
+              ],
               maximumFileSizeToCacheInBytes: 10e6,
             },
             pwaAssets: {
