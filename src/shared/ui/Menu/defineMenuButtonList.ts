adf5b8763f5297937885e1687ce6178e23bc7078
vyachean <vyachean@gmail.com>
2026-02-13 13:43:36 +0400

    feat: refactor RepoExplorerPane and add context buttons for document and file entries

--- src/shared/ui/Menu/defineMenuButtonList.ts
+++ src/shared/ui/Menu/defineMenuButtonList.ts
@@ -1,4 +1,6 @@
-import type { MenuButtonList } from './types';
+import type { MenuButtonDescription, MenuButtonList } from './types';
 
 export const defineMenuButtonList = <T extends MenuButtonList>(value: T): T =>
   value;
+
+export const defineMenuButton = <T extends MenuButtonDescription>(v: T) => v;
