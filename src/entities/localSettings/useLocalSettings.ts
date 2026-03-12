c7e458e059d7c159b810c818066f29675e426171
vyachean <vyachean@gmail.com>
2026-03-12 17:23:41 +0400

    feat: integrate local settings for pane width management in MDSplitLayout component

--- src/entities/localSettings/useLocalSettings.ts
+++ src/entities/localSettings/useLocalSettings.ts
@@ -1,26 +1,38 @@
 import { createGlobalState } from '@vueuse/core';
 import { useIDBKeyval } from '@vueuse/integrations/useIDBKeyval';
+import { toMerged } from 'es-toolkit';
 
 interface SettingsStorage {
   showPerformance: boolean;
   showAutomergeFiles: boolean;
+  panesWidth: number[];
 }
 
 export const useLocalSettings = createGlobalState(() => {
   const SETTINGS_DESCRIPTION: Record<keyof SettingsStorage, string> = {
     showPerformance: 'Show performance layer',
     showAutomergeFiles: 'Show *.automerge files in explorer',
+    panesWidth: 'Store panes width',
   };
 
   const SETTINGS_LABEL: Record<keyof SettingsStorage, string> = {
     showPerformance: 'Performance layer',
     showAutomergeFiles: 'Show *.automerge files',
+    panesWidth: 'Pane widths',
   };
 
-  const { data: storage } = useIDBKeyval<Partial<SettingsStorage>>(
+  const initialSettings: () => SettingsStorage = () => ({
+    showAutomergeFiles: false,
+    showPerformance: false,
+    panesWidth: [],
+  });
+
+  const { data: storage, set } = useIDBKeyval<SettingsStorage>(
     'settings',
-    {},
+    initialSettings(),
   );
+
+  void set(toMerged(initialSettings(), storage.value));
 
   return { settings: storage, SETTINGS_DESCRIPTION, SETTINGS_LABEL };
 });
