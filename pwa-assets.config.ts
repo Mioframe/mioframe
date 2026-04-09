baae73a4dfaa3f99409c10904d2792ec88225c34
vyachean <vyachean@gmail.com>
2026-04-09 16:25:18 +0400

    refactor: improve code formatting and linting configurations

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,12 +1,9 @@
-import {
-  defineConfig,
-  minimal2023Preset,
-} from '@vite-pwa/assets-generator/config';
+import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
   headLinkOptions: {
     preset: '2023',
   },
-  preset: minimal2023Preset,
   images: 'public/favicon.svg',
+  preset: minimal2023Preset,
 });
