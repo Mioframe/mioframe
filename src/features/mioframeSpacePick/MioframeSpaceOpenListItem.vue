69d90aca1e2da8b7316ca784a45d301553fd9458
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-19 15:25:22 +0400

    refactor: make Mioframe open flow own its list item

---
+++ src/features/mioframeSpacePick/MioframeSpaceOpenListItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { MDSymbol } from '@shared/ui/Icon';
+import { MDListItem } from '@shared/ui/Lists';
+import { usePickMioframeSpace } from './usePickMioframeSpace';
+
+const { loading, openSpace } = usePickMioframeSpace();
+</script>
+
+<template>
+  <MDListItem
+    is="button"
+    headline="Open space"
+    supporting-text="Choose a folder that already contains a Mioframe space."
+    :lines="2"
+    :disabled="loading"
+    @click="openSpace"
+  >
+    <template #leadingIcon>
+      <MDSymbol name="folder_open" />
+    </template>
+  </MDListItem>
+</template>
