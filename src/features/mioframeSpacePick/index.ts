b6e4f13fa8941e112fbce024a1e23ce59274a9b7
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-19 15:46:32 +0400

    refactor: export split Mioframe space composables

--- src/features/mioframeSpacePick/index.ts
+++ src/features/mioframeSpacePick/index.ts
@@ -1,4 +1,6 @@
 export { usePickMioframeSpace } from './usePickMioframeSpace';
+export { useCreateMioframeSpace } from './useCreateMioframeSpace';
+export { useOpenMioframeSpace } from './useOpenMioframeSpace';
 export { default as MioframeSpaceCreateDialog } from './MioframeSpaceCreateDialog.vue';
 export { default as MioframeSpaceCreateListItem } from './MioframeSpaceCreateListItem.vue';
 export { default as MioframeSpaceOpenListItem } from './MioframeSpaceOpenListItem.vue';
