53d5ab979a92e8cfcdb7e15e7d93ef4366a74c58
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-19 15:25:44 +0400

    refactor: expose Mioframe space list item features

--- src/features/mioframeSpacePick/index.ts
+++ src/features/mioframeSpacePick/index.ts
@@ -1,3 +1,4 @@
 export { usePickMioframeSpace } from './usePickMioframeSpace';
 export { default as MioframeSpaceCreateDialog } from './MioframeSpaceCreateDialog.vue';
-export { default as MioframeSpacePickDialogs } from './MioframeSpacePickDialogs.vue';
+export { default as MioframeSpaceCreateListItem } from './MioframeSpaceCreateListItem.vue';
+export { default as MioframeSpaceOpenListItem } from './MioframeSpaceOpenListItem.vue';
