7ca4c8582d61245d77f78c8952720b5326215d4d
vyachean <vyachean@gmail.com>
2026-07-06 18:35:40 +0400

    build: set build target based on browserslist configuration
    - integrate browserslist and browserslist-to-esbuild for target resolution
    - ensure compatibility with specified browser versions in the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import browserslist from 'browserslist';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 import type { PluginOption } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import { dependencies, devDependencies, version } from './package.json';
@@ -13,6 +15,7 @@ import {
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, isPreview }) => {
+  const buildTarget = browserslistToEsbuild(browserslist(undefined, { path: process.cwd() }));
   const env = loadEnv(mode, process.cwd(), '');
   const isPreviewBuild = !!isPreview;
   const isStorybookBuild = process.env.APP_STORYBOOK === '1';
@@ -81,6 +84,7 @@ export default defineConfig(({ mode, isPreview }) => {
       alias: getResolveAlias(),
     },
     build: {
+      target: buildTarget,
       sourcemap: sentryPlugins.length ? 'hidden' : false,
       assetsDir: 'assets',
       minify: mode === 'production' || isPreviewBuild ? 'terser' : false,
